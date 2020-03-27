659dd7e60dab18b14a10e4a9ee31d00e09cd11dd
kyrylo_kundik <goliathusua@gmail.com>
2020-03-27 11:28:29 +0200

    started working on api client for bot;

---
+++ bot/api_client.py
@@ -0,0 +1,44 @@
+import asyncio
+
+from aiogram import types
+from aiohttp import ClientSession, ClientResponseError
+
+
+class ApiClientError(Exception):
+    pass
+
+
+class ApiClient:
+    def __init__(self, api_url, loop=None):
+        self._loop = loop or asyncio.get_event_loop()
+
+        self._session = ClientSession(raise_for_status=True)
+        self._api_url = api_url
+
+    async def fetch(
+            self,
+            method: str = "/",
+            verb: str = "POST",
+            params: dict = None,
+            headers: dict = None
+    ):
+        url = self._api_url + method
+
+        if verb == "POST":
+            payload = params
+            params = None
+        else:
+            payload = None
+
+        try:
+            async with self._session.request(
+                    verb, url, data=payload, params=params, headers=headers
+            ) as response:
+                response = await response.json()
+
+            return response
+        except ClientResponseError as e:
+            raise ApiClientError from e
+
+    async def add_user(self, user: types.User):
+        pass
